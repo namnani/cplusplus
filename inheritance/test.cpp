ec7031ee0e3aad6166d238e2541846768a0035a6
namnani <okokoknow@naver.com>
2020-08-15 03:58:25 +0900

    [C++] Constructor, Deconstructor call order in case multiple inheritance.

---
+++ inheritance/test.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+class A {
+	public:
+		A() { cout << "A" << endl; }
+		virtual ~A() { cout << "~A" << endl; }
+};
+
+class B : public A {
+	public:
+		B() { cout << "B" << endl; }
+		virtual ~B() { cout << "~B" << endl; }
+};
+
+class C : public B {
+	public:
+		C() { cout << "C" << endl; }
+		virtual ~C() { cout << "~C" << endl; }
+};
+
+int main(void)
+{
+	A* a = new C();
+
+	delete a;
+
+	return 0;
+}
